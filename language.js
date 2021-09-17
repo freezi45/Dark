79518e04fbca71893fee28b5170c2b120c5dbe89
freezi45 <90923836+freezi45@users.noreply.github.com>
2021-09-17 22:57:00 +0530

    Create language.js

---
+++ language.js
@@ -0,0 +1,33 @@
+/* Copyright (C) 2021 TENUX-Neotro.
+Licensed under the  GPL-3.0 License;
+you may not use this file except in compliance with the License.
+NEOTROX - TEENUHX
+Wa.me/+94766598862
+*/
+
+const Config = require('./config');
+const fs = require('fs');
+const chalk = require('chalk');
+
+if (fs.existsSync('./language/' + Config.LANG + '.json')) {
+    console.log(
+        chalk.green.bold('Loading ' + Config.LANG + ' language...')
+    );
+
+    var json = JSON.parse(fs.readFileSync('./language/' + Config.LANG + '.json'));
+} else {
+    console.log(
+        chalk.red.bold('You entered an invalid language. Sinhala language was chosen.')
+    );
+
+    var json = JSON.parse(fs.readFileSync('./language/SI.json'));
+}
+
+function getString(file) {
+    return json['STRINGS'][file];
+}
+
+module.exports = {
+    language: json,
+    getString: getString
+}
